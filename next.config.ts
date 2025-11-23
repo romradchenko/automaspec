7bad3bbe2b7a624981fae44aaf09d290f29e229c
Aliaksandr <grubian2@gmail.com>
2025-11-23 16:57:21 +0200

    fix docker deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+    output: 'standalone',
     experimental: {
         browserDebugInfoInTerminal: true,
         typedEnv: true,
