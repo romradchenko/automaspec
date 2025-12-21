654cc11e0495d19aebe504b14955a83fc93cdeb1
Aliaksandr <grubian2@gmail.com>
2025-12-21 17:40:29 +0200

    less comments more work

--- lib/orpc/orpc.server.ts
+++ lib/orpc/orpc.server.ts
@@ -5,18 +5,7 @@ import { headers } from 'next/headers'
 import { router } from '@/orpc/routes'
 
 globalThis.$client = createRouterClient(router, {
-    /**
-     * Provide initial context if needed.
-     *
-     * Because this client instance is shared across all requests,
-     * only include context that's safe to reuse globally.
-     * For per-request context, use middleware context or pass a function as the initial context.
-     */
     context: async () => ({
-        // TODO: should i add session here?
-        // session: await auth.api.getSession({
-        //     headers: await headers()
-        // }),
-        headers: await headers() // provide headers if initial context required
+        headers: await headers()
     })
 })
